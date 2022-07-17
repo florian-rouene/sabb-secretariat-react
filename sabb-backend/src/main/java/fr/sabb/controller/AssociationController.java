122e0fef203b30d040c4e3b1b6c941804b28ae3b
florian-rouene <florian.rouene@mailo.com>
2022-07-17 16:27:34 +0200

    Formulaire association

--- sabb-backend/src/main/java/fr/sabb/controller/AssociationController.java
+++ sabb-backend/src/main/java/fr/sabb/controller/AssociationController.java
@@ -4,19 +4,26 @@ import fr.sabb.data.object.Association;
 import fr.sabb.service.assocation.AssociationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/associations")
 public class AssociationController {
 
     @Autowired
     private AssociationService associationService;
 
-    @GetMapping("/associations")
-    public List<Association> getAssociations(@RequestParam(value = "name") String name) {
+    @GetMapping
+    public List<Association> getAssociations() {
         return associationService.getAllActive();
+    }
+
+    @GetMapping("/{id}")
+    public Association getAssociation(@PathVariable int id) {
+        return associationService.getById(id).orElseThrow(RuntimeException::new);
     }
 }
